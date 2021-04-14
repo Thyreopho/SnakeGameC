9316a230cc6d4124a6e58fc8abfa7c919fada73a
Felipe Lacerda Tertuliano <felipeltertuliano@gmail.com>
2021-04-14 01:12:40 -0300

    Adicionando comentarios no codigo

--- environment.h
+++ environment.h
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 
 Map* createMap(unsigned int height, unsigned int width) {
-	Map* map = (Map*) malloc(sizeof(Map));
+	Map* map = (Map*) malloc(sizeof(Map)); //Instancia-se um novo mapa com os dados informados
 	map->height = height;
 	map->width = width;
-	map->grid = (Chunk***) calloc(height, sizeof(Chunk**));
 
+	//Instancia por completo a matriz do mapa, preenchendo-a com NULLs
+	map->grid = (Chunk***) calloc(height, sizeof(Chunk**));
 	for (unsigned int y = 0; y < height; y++) {
 		map->grid[y] = (Chunk**) calloc(width, sizeof(Chunk*));
 		for (unsigned int x = 0; x < width; x++) {
@@ -16,29 +17,33 @@ Map* createMap(unsigned int height, unsigned int width) {
 	return map;
 }
 
-Chunk* getChunk(Map* map, unsigned int x, unsigned int y) {
-	return map->grid[y][x];
-}
-                                                                
-Chunk* placeChunk(Map* map, char icon, unsigned int x, unsigned int y) {
-	Chunk* result = (Chunk*) malloc(sizeof(Chunk));
+Chunk* createChunk(char icon, unsigned int x, unsigned int y) {
+	Chunk* result = (Chunk*) malloc(sizeof(Chunk)); //Instancia-se um novo Chunk com os dados informados
 	result->x = x;
 	result->y = y;
 	result->icon = icon;
-	map->grid[y][x] = result;
 
 	return result;
 }
+
+Chunk* getChunk(Map* map, unsigned int x, unsigned int y) {
+	return map->grid[y][x]; //Retorna o Chunk na posicao informada
+}
+
+void setChunk(Map* map, Chunk* chunk) {
+	free(getChunk(map, chunk->x, chunk->y)); //Deleta antigo Chunk
+	map->grid[chunk->y][chunk->x] = chunk; //Posiciona o novo Chunk
+}
                                                                 
 Chunk* removeChunk(Map* map, unsigned int x, unsigned int y) {
-	Chunk* result = map->grid[y][x];
-	map->grid[y][x] = NULL;
-	return result;
+	Chunk* result = getChunk(map, x, y); //Pega elemento que sera removido
+	map->grid[y][x] = NULL; //Define posicao com valor igual a NULL
+	return result; //Retorna elemento removodo
 }
 
 
 Chunk* replaceChunk(Map* map, Chunk* chunk) {
-	Chunk* removed = removeChunk(map, chunk->x, chunk->y);
-	map->grid[chunk->y][chunk->x] = chunk;
-	return removed;
+	Chunk* removed = removeChunk(map, chunk->x, chunk->y); //Remove elemento na mesma posicao do Chunk informado
+	setChunk(map, chunk); //Posiciona novo Chunk
+	return removed; //Retorna elemento removido
 }
--- main.c
+++ main.c
@@ -9,28 +9,28 @@
 
 int main (int argc, char** argv) {
 
-	Map* map = createMap(HEIGHT, WIDTH);
-	for (unsigned int i = 0; i < WIDTH; i++) {
-		placeChunk(map, WALL, i, 0);
-		placeChunk(map, WALL, i, HEIGHT-1);
+	Map* map = createMap(HEIGHT, WIDTH); //Criar mapa
+	for (unsigned int i = 0; i < WIDTH; i++) { //Posicioar paredes horizontais do mapa
+		setChunk(map, createChunk(WALL, i, 0));
+		setChunk(map, createChunk(WALL, i, HEIGHT-1));
 	}
 
 	unsigned int limit = HEIGHT - 1;
-	for (unsigned int i = 1; i < limit; i++) {
-		placeChunk(map, WALL, 0, i);
-		placeChunk(map, WALL, WIDTH-1, i);
+	for (unsigned int i = 1; i < limit; i++) { //Posicionar paredes verticais do mapa
+		setChunk(map, createChunk(WALL, 0, i));
+		setChunk(map, createChunk(WALL, WIDTH-1, i));
 	}
-
-	Player* player = createPlayer(map);
+	
+	Player* player = createPlayer(map); //Criar jogador
 	
 	char direction;
 	placeBerry(map);
 	do {
-		render(map, player);
+		render(map, player); //Renderizar mapa
 		system("/bin/stty raw");
-		direction = getchar();
+		direction = getchar(); //Ler somente um char
 		system("/bin/stty cooked");
-	} while(move(map, player, direction));
+	} while(move(map, player, direction)); //mover jogador por enquanto nao e fim de jogo
 
 	return 0;
 }
